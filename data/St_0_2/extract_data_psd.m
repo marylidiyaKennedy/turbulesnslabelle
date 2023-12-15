ebb35a5bdffda1bd38671afbe7f255ab32b0f91c
amalroymurali <roy.amalmurali@gmail.com>
2023-12-15 17:25:08 +0100

    Adding TowneSPOD type data

---
+++ data/St_0_2/extract_data_psd.m
@@ -0,0 +1,16 @@
+uiopen('/home/amurali/Desktop/Sergey/GPANNpaper/Original_space/St_0_2/220308_SP7_spod_m0_St02_NORM_M50G50_Strat15_a4_1e-2_a8_1e-1_a10_0_r4g50_FFT_Test.fig',1)
+
+fig=gca;
+xdata2 = fig.Children(1).XData;
+ydata2 = fig.Children(1).YData;
+
+data = [xdata2' ydata2'];
+
+csvwrite('HyGP_psd.csv', data);
+
+xdata3 = fig.Children(2).XData;
+ydata3 = fig.Children(2).YData;
+
+data = [xdata3' ydata3'];
+
+csvwrite('True_psd.csv', data);
---
+++ data/St_1_0/extract_data_fig6_1_0.m
@@ -0,0 +1,15 @@
+uiopen('/home/amurali/Desktop/Sergey/GPANNpaper/Original_space/St_1_0/220308_SP7_spod_m0_St1_NORM_M50G50_Strat15_a4_1e-2_a8_1e-1_a10_0_t2_R4G50_TEST.fig',1)
+fig=gca;
+xdata2 = fig.Children(2).XData;
+ydata2 = fig.Children(2).YData;
+
+data = [xdata2' ydata2'];
+
+csvwrite('HyGP.csv', data);
+
+xdata3 = fig.Children(3).XData;
+ydata3 = fig.Children(3).YData;
+
+data = [xdata3' ydata3'];
+
+csvwrite('True.csv', data);
